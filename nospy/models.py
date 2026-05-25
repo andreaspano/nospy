448027a41aff2d5255824c06ff2df724a3e65c96
andreaspano <andrea@spano.it>
2026-05-25 22:02:11 +0200

    added more parameters to nhits

--- nospy/models.py
+++ nospy/models.py
@@ -40,15 +40,54 @@ def get_nhits_config(test: bool = False):
         }
 
     return {
+        # quanta storia passata il modello vede
         "input_size": tune.choice([20, 40, 60, 80]),
         "max_steps": tune.choice([300, 500, 700]),
         "learning_rate": tune.choice([1e-3, 5e-4, 1e-4]),
-        "batch_size": tune.choice([16, 32, 64, 128, 256, 512, 1024, 2048, 4096]),
-        "windows_batch_size": tune.choice([1024, 2048, 4096, 8192, 16384]),
-        "n_pool_kernel_size": tune.choice([[2, 2, 1], [3, 2, 1]]),
-        "n_freq_downsample": tune.choice([[8, 4, 1], [4, 2, 1]]),
+        # In NHITS, this is the number of series for which we compute the loss 
+        # and update the model parameters in each training step.
+        # batch size troppo grossi rischiano di edere variazioni importanti tra un passo e l'altro, 
+        # ma batch size troppo piccoli possono rendere l'addestramento instabile.
+
+        "batch_size": tune.choice([16, 32, 64, 128]), 
+        # number of windows processed simultaneously during training. 
+        # Higher values can speed up training but require more memory.
+        "windows_batch_size": tune.choice([512, 1024, 2048]), 
+        
+        # => quanto "zoom out" faccio sul passato
+         "n_pool_kernel_size": tune.choice([
+            [1, 1, 1],
+            [2, 2, 1],
+            [3, 2, 1],
+        ]), 
+        
+        # => quanto dettaglio/rumore permetto nella previsione
+            "n_freq_downsample": tune.choice([
+            [1, 1, 1],
+            [2, 2, 1],
+            [4, 2, 1],
+        ]),
+
+   
         "scaler_type": tune.choice(["robust", "standard"]),
         "random_seed": tune.choice([42, 123, 2026]),
+        
+        # dropout applicato ai layer MLP del modello
+        # quanti neuroni vengono "spenti" casualmente durante il training
+        "dropout_prob_theta": tune.choice([0.0, 0.1, 0.2]),
+        
+        "activation": tune.choice(["ReLU", "LeakyReLU", "SELU"]),
+
+        # numero di neuroni nei layer MLP del modello.
+        "mlp_units": tune.choice([
+            [[128, 128], [128, 128], [128, 128]],
+            [[256, 256], [256, 256], [256, 256]],
+
+        ]),
+        # ogni 100 step:
+        # il training si ferma temporaneamente
+        # il modello viene valutato sul validation set
+        "val_check_steps": tune.choice([50, 100]),
     }
 
 
